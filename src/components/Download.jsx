f369182ad19c792076edea206acd7fb28dabbadd
Itodo Simon <itodosimonitodo1@gmail.com>
2022-04-15 09:20:26 +0100

    first commit

---
+++ src/components/Download.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import styles from '../styles/Global';
+import assets from '../assets';
+
+const Download = () => {
+  const link = 'https://github.com/Itodo-S/ReactNativeMarket'
+  return (
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Download the Source Code</h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>Get the full source code on GitHub</p>
+        </div>
+        <button onClick={() => window.open(link, "_blank")} className={styles.btnPrimary}>Source Code</button>
+        <div className={styles.flexCenter}>
+          <img 
+            src={assets.scene}
+            alt="download_png"
+            className={styles.fullImg}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Download
